e65636476135bf3ec748a466d156723ac9787651
viktor7cr7 <volkovatatiana1919@gmail.com>
2024-03-10 02:01:10 +0300

    fix front-end

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
   
 app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
 })
 
 app.use(errorHandlerMiddleware)
